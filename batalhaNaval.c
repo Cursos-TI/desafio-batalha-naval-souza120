112e4aacf4454764bab69c023221f32d95d580aa
souza120 <mdefreitas1209@gmail.com>
2025-09-05 01:49:27 +0000

    criei o desafio Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+int main() {
+    int tabuleiro[5][5] = {0}; 
+    int i, j;
+
+    // Navio horizontal (linha 1, colunas 0 a 3)
+    for (j = 0; j < 4; j++) {
+        tabuleiro[1][j] = 3;
+        printf("Navio horizontal em: (%d,%d)\n", 1, j);
+    }
+
+    // Navio vertical (coluna 4, linhas 0 a 3)
+    for (i = 0; i < 4; i++) {
+        tabuleiro[i][4] = 3;
+        printf("Navio vertical em: (%d,%d)\n", i, 4);
+    }
+
+    return 0;
+}
+
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
@@ -35,6 +54,3 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
-
-    return 0;
-}
