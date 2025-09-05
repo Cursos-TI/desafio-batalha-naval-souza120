e52d5abb19092915c70fe761bc39a7665e8c92a8
souza120 <mdefreitas1209@gmail.com>
2025-09-05 02:13:31 +0000

    criei printf novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int tabuleiro[5][5] = {0}; 
+    int tabuleiro[5][5] = {0};
     int i, j;
 
     // Navio horizontal (linha 1, colunas 0 a 3)
@@ -9,48 +9,10 @@ int main() {
         tabuleiro[1][j] = 3;
         printf("Navio horizontal em: (%d,%d)\n", 1, j);
     }
-
     // Navio vertical (coluna 4, linhas 0 a 3)
     for (i = 0; i < 4; i++) {
         tabuleiro[i][4] = 3;
         printf("Navio vertical em: (%d,%d)\n", i, 4);
     }
-
     return 0;
 }
-
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
